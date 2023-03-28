f48c4c0fae11da7cd35d89ea6f7db174b3521a61
Bruce <bruce.hillsworth@gmail.com>
2023-03-27 21:20:27 -0600

    Added submitData function that posts content via fetch

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(usersName, usersEmail) {
+  const configureObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: usersName,
+      email: usersEmail,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configureObject)
+    .then((response) => response.json())
+    .then((object) => {
+      document.body.innerHTML = `${object.id}`;
+    })
+    .catch((error) => {
+      document.body.innerHTML = `${error.message}`;
+    });
+}
